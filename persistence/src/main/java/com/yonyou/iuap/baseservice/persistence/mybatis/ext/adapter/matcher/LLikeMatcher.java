6d4a9f63d3603de323995f8a914fed9b78dae471
lengleng982 <lengleng982@sina.com>
2018-06-24 10:56:15 +0800

    添加条件匹配

---
+++ persistence/src/main/java/com/yonyou/iuap/baseservice/persistence/mybatis/ext/adapter/matcher/LLikeMatcher.java
@@ -0,0 +1,26 @@
+package com.yonyou.iuap.baseservice.persistence.mybatis.ext.adapter.matcher;
+
+import java.lang.reflect.Field;
+import com.yonyou.iuap.baseservice.persistence.mybatis.ext.support.Match;
+import com.yonyou.iuap.baseservice.persistence.mybatis.ext.support.ParamUtil;
+import com.yonyou.iuap.baseservice.persistence.mybatis.ext.utils.FieldUtil;
+
+public class LLikeMatcher implements Matcher{
+
+	@Override
+	public Match getMatch() {
+		return Match.LLIKE;
+	}
+
+	@Override
+	public String buildCondition(Field field, String prefix) {
+		StringBuilder strb = new StringBuilder("\r\n\t<if test=\"");
+		String fieldName = ParamUtil.contactParam(prefix, field.getName());
+		strb.append(fieldName).append("!=null \">\r\n");
+		strb.append("\t\t and ").append(FieldUtil.getColumnName(field)).append(" like CONCAT('%', #{")
+			.append(fieldName).append("})\r\n");
+		strb.append("\t</if>\r\n");
+		return strb.toString();
+	}
+
+}
