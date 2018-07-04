dd8493025c687d0a94e6fd94be2423668bca3a0f
lengleng982 <lengleng982@sina.com>
2018-07-04 08:42:19 +0800

    添加Excel操作处理工具类

---
+++ service/src/test/java/com/yonyou/iuap/baseservice/service/Entity.java
@@ -0,0 +1,28 @@
+package com.yonyou.iuap.baseservice.service;
+
+public class Entity {
+	
+	private int id;
+	private String code;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
