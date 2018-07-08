60d5ad9300ab13eb2a1211dfdfbb36eb2de2f435
huangddm <huangddm@yonyou.com>
2018-07-08 11:21:45 +0800

    规范类型。

--- entity/src/main/java/com/yonyou/iuap/baseservice/entity/annotation/RefType.java
+++ entity/src/main/java/com/yonyou/iuap/baseservice/entity/annotation/RefType.java
@@ -1,7 +1,7 @@
 package com.yonyou.iuap.baseservice.entity.annotation;
 
 public enum RefType {
-    Single(0, "单选"), Multi(1, "多选");
+    Tree(1, "树形"), Single(2, "单表"), TreeCard(3, "树卡型"), Multi(4, "多选"), Shuttle(5, "穿梭框"), MFilter(6, "多过滤项");
     private int type;
     private String typeName;
 
