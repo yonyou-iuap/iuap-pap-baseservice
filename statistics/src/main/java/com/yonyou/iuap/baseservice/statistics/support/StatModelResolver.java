7685072af775e4ed2f050645eb5a7f3e0a72dca7
leon <yangliangs@yonyou.com>
2018-11-30 19:39:50 +0800

    扩充了统计模型的扫描范围

--- statistics/src/main/java/com/yonyou/iuap/baseservice/statistics/support/StatModelResolver.java
+++ statistics/src/main/java/com/yonyou/iuap/baseservice/statistics/support/StatModelResolver.java
@@ -3,22 +3,19 @@ package com.yonyou.iuap.baseservice.statistics.support;
 import cn.hutool.core.map.CaseInsensitiveMap;
 import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.ReflectUtil;
-import com.yonyou.iuap.baseservice.entity.Model;
 import com.yonyou.iuap.baseservice.persistence.mybatis.ext.utils.EntityUtil;
 import com.yonyou.iuap.baseservice.persistence.mybatis.ext.utils.FieldUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Lazy;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.persistence.Table;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 /**
  * 通过全局性扫描Entity及注解@StatisticField，得到整体的统计模型，后续可自动拼凑sql
@@ -28,6 +25,7 @@ import java.util.concurrent.Executors;
 public class StatModelResolver {
     private static Logger logger = LoggerFactory.getLogger(StatModelResolver.class);
     private static Map<String, StatModel> modelCache;
+    private static final String SCAN_ROOT ="com,cn,org,net,io,de";//默认的模型扫描范围
 
     /**
      * 优化性能，异步方式扫描
@@ -39,30 +37,35 @@ public class StatModelResolver {
           @Override
           public void run(){
               modelCache = new CaseInsensitiveMap<>();
-              String basePackages = "com"; //TODO 改成更广泛的扫描范围
-              Set<Class<?>> classes = ClassUtil.scanPackageByAnnotation(basePackages, Table.class);
+              String[] basePackages = SCAN_ROOT.split(",");
+              Arrays.stream(basePackages).parallel().forEach(
+                      (basePackage)->{
+                          Set<Class<?>> classes = ClassUtil.scanPackageByAnnotation(basePackage, Table.class);
+                          for (Class clz : classes) {
+                              logger.debug("Resolving model class:" + clz);
+                              Field[] fields = ReflectUtil.getFields(clz);
+                              StatModel statModel = null;
+                              for (Field f : fields) {
+                                  if (f.getAnnotation(StatisticsField.class) != null) {
+                                      statModel = statModel == null ? new StatModel() : statModel;
+                                      statModel.getStatColumnsFields().put(FieldUtil.getColumnName(f), f.getName());
+                                      statModel.getStatColumnsFunctions().put(FieldUtil.getColumnName(f), f.getAnnotation(StatisticsField.class).functions());
+                                  }
+                              }
+                              if (statModel != null) {
+                                  statModel.setCode(clz.getSimpleName());
+                                  statModel.setmClass(clz);
+                                  statModel.setTableName(EntityUtil.getTableName(clz));
+                                  logger.info("StatModelResolver caching model:" + statModel);
+                                  synchronized (this){
+                                      modelCache.put(clz.getSimpleName(), statModel);
+                                  }
+                              }
+                          }
 
-              for (Class clz : classes) {
-                  logger.debug("Resolving model class:" + clz);
-                  Field[] fields = ReflectUtil.getFields(clz);
-                  StatModel statModel = null;
-                  for (Field f : fields) {
-                      if (f.getAnnotation(StatisticsField.class) != null) {
-                          statModel = statModel == null ? new StatModel() : statModel;
-                          statModel.getStatColumnsFields().put(FieldUtil.getColumnName(f), f.getName());
-                          statModel.getStatColumnsFunctions().put(FieldUtil.getColumnName(f), f.getAnnotation(StatisticsField.class).functions());
 
                       }
-                  }
-                  if (statModel != null) {
-                      statModel.setCode(clz.getSimpleName());
-                      statModel.setmClass(clz);
-                      statModel.setTableName(EntityUtil.getTableName(clz));
-                      logger.info("StatModelResolver caching model:" + statModel);
-                      modelCache.put(clz.getSimpleName(), statModel);
-                  }
-              }
-
+              );
               logger.info("StatModelResolver cached model:" + modelCache);
           }
 
