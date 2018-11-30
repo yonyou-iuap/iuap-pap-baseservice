fc9f6fca1e68aa6e4382d356cdb66455ee411508
leon <yangliangs@yonyou.com>
2018-11-30 09:59:55 +0800

    make statController in param   easier to construct

--- statistics/src/main/java/com/yonyou/iuap/baseservice/statistics/controller/StatCommonController.java
+++ statistics/src/main/java/com/yonyou/iuap/baseservice/statistics/controller/StatCommonController.java
@@ -1,15 +1,14 @@
 package com.yonyou.iuap.baseservice.statistics.controller;
 
 import com.yonyou.iuap.base.web.BaseController;
-import com.yonyou.iuap.baseservice.persistence.support.PageRequestAndSearchParams;
 import com.yonyou.iuap.baseservice.statistics.service.StatCommonService;
+import com.yonyou.iuap.baseservice.statistics.support.StatParam;
 import com.yonyou.iuap.mvc.type.SearchParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.jaxb.SpringDataJaxb;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @Controller
-@RequestMapping(value = "/base/statistics")
+@RequestMapping(value = "/base/stats")
 public class StatCommonController extends BaseController {
     private Logger logger = LoggerFactory.getLogger(StatCommonController.class);
 
@@ -28,8 +27,10 @@ public class StatCommonController extends BaseController {
 
     @RequestMapping(value = {"/page/{modelCode}"}, method = {RequestMethod.POST})
     @ResponseBody
-    public Object page(@PathVariable String modelCode, PageRequest pageRequest,@RequestBody SearchParams searchParams) {
-
+    public Object page(@PathVariable String modelCode, PageRequest pageRequest,@RequestBody Map<String,Object> searchMap) {
+        logger.debug("StatCommonController getting searchParams:"+searchMap);
+        SearchParams searchParams = new SearchParams();
+        searchParams.setSearchMap(searchMap);
         Page  page = this.service.selectAllByPage(pageRequest, searchParams,modelCode);
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("data", page);
@@ -38,7 +39,10 @@ public class StatCommonController extends BaseController {
 
     @RequestMapping(value = "/list/{modelCode}" ,method = RequestMethod.POST )
     @ResponseBody
-    public Object list( @PathVariable String modelCode, PageRequest pageRequest,@RequestBody SearchParams searchParams) {
+    public Object list( @PathVariable String modelCode, PageRequest pageRequest,@RequestBody Map<String,Object> searchMap) {
+        logger.debug("StatCommonController getting searchParams:"+searchMap);
+        SearchParams searchParams = new SearchParams();
+        searchParams.setSearchMap(searchMap);
         List list = this.service.findAll(pageRequest, searchParams,modelCode);
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("data", list);
