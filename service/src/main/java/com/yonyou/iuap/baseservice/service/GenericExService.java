521f4f4a195f2b3c6717dc6a0c73158042bba549
leon <yangliangs@yonyou.com>
2018-12-21 18:29:04 +0800

    GenericExService实现国际化

--- service/src/main/java/com/yonyou/iuap/baseservice/service/GenericExService.java
+++ service/src/main/java/com/yonyou/iuap/baseservice/service/GenericExService.java
@@ -1,5 +1,6 @@
 package com.yonyou.iuap.baseservice.service;
 
+import com.yonyou.iuap.i18n.MessageSourceUtil;
 import java.io.Serializable;
 
 import org.slf4j.Logger;
@@ -17,85 +18,85 @@ import com.yonyou.iuap.persistence.vo.pub.BusinessException;
  * 2018年6月12日
  */
 public abstract class GenericExService<T extends Model & LogicDel> extends GenericService<T>{
-	
-	private Logger log = LoggerFactory.getLogger(GenericExService.class);
 
-	/**
-	 * 新增保存数据
-	 * @param entity
-	 * @return
-	 */
-	public T insert(T entity) {
-		if(entity != null) {
-			entity.setDr(LogicDel.NORMAL);
-			return super.insert(entity);
-		}else {
-			throw new BusinessException("新增保存数据出错，对象为空!");
-		}
-	}
+    private Logger log = LoggerFactory.getLogger(GenericExService.class);
 
-	/**
-	 * 更新保存数据
-	 * @param entity
-	 * @return
-	 */
-	@Override
-	public T update(T entity) {
-		if(entity != null) {
-			entity.setDr(LogicDel.NORMAL);
-			return super.update(entity);
-		}else {
-			throw new BusinessException("更新保存数据出错，对象为空!");
-		}
-	}
-	
-	/**
-	 * 逻辑删除
-	 * @param entity
-	 * @return
-	 */
-	public int update4LogicDel(T entity) {
-		if(entity != null) {
-			entity.setDr(LogicDel.DELETED);
-			int count = this.genericMapperEx.update(entity);
-			if(count == 1) {
-				return count;
-			}else {
-				log.error("删除数据出错,记录数="+count+"\r\n"+JSON.toJSONString(entity));
-				throw new BusinessException();
-			}
-		}else {
-			throw new BusinessException("数据对象为空,无法删除!");
-		}
-	}
+    /**
+     * 新增保存数据
+     * @param entity
+     * @return
+     */
+    public T insert(T entity) {
+        if(entity != null) {
+            entity.setDr(LogicDel.NORMAL);
+            return super.insert(entity);
+        }else {
+            throw new BusinessException(MessageSourceUtil.getMessage("ja.bas.ser1.0001", "新增保存数据出错，对象为空!"));
+        }
+    }
 
-	/**
-	 * 覆盖父方法，调整为逻辑删除
-	 * @param entity
-	 * @return
-	 */
-	@Override
-	public int delete(T entity) {
-		return this.update4LogicDel(entity);
-	}
+    /**
+     * 更新保存数据
+     * @param entity
+     * @return
+     */
+    @Override
+    public T update(T entity) {
+        if(entity != null) {
+            entity.setDr(LogicDel.NORMAL);
+            return super.update(entity);
+        }else {
+            throw new BusinessException(MessageSourceUtil.getMessage("ja.bas.ser1.0002", "更新保存数据出错，对象为空!"));
+        }
+    }
 
-	/**
-	 * 覆盖父方法，调整为逻辑删除
-	 * @param id
-	 */
-	@Override
-	public int delete(Serializable id) {
-		T entity = this.findById(id);
-		return this.delete(entity);
-	}
+    /**
+     * 逻辑删除
+     * @param entity
+     * @return
+     */
+    public int update4LogicDel(T entity) {
+        if(entity != null) {
+            entity.setDr(LogicDel.DELETED);
+            int count = this.genericMapperEx.update(entity);
+            if(count == 1) {
+                return count;
+            }else {
+                log.error(MessageSourceUtil.getMessage("ja.bas.ser1.0003", "删除数据出错,记录数=")+count+"\r\n"+JSON.toJSONString(entity));
+                throw new BusinessException();
+            }
+        }else {
+            throw new BusinessException(MessageSourceUtil.getMessage("ja.bas.ser1.0004", "数据对象为空,无法删除!"));
+        }
+    }
+
+    /**
+     * 覆盖父方法，调整为逻辑删除
+     * @param entity
+     * @return
+     */
+    @Override
+    public int delete(T entity) {
+        return this.update4LogicDel(entity);
+    }
+
+    /**
+     * 覆盖父方法，调整为逻辑删除
+     * @param id
+     */
+    @Override
+    public int delete(Serializable id) {
+        T entity = this.findById(id);
+        return this.delete(entity);
+    }
 
 
-	/***************************************************/
-	protected GenericExMapper<T> genericMapperEx;
+    /***************************************************/
+    protected GenericExMapper<T> genericMapperEx;
 
-	public void setIbatisMapperEx(GenericExMapper<T> mapper) {
-		this.genericMapperEx = mapper;
-		super.setGenericMapper(mapper);
-	}
+    public void setIbatisMapperEx(GenericExMapper<T> mapper) {
+        this.genericMapperEx = mapper;
+        super.setGenericMapper(mapper);
+    }
 
 }
