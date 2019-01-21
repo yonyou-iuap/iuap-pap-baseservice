276d28e6c842bc476e00155ab52cdb89ec507615
leon <yangliangs@yonyou.com>
2019-01-21 19:19:08 +0800

    finish the combination of 4 jar into 1 core jar

--- persistence/src/main/java/com/yonyou/iuap/baseservice/persistence/support/CustomDeletable.java
+++
@@ -1,9 +0,0 @@
-package com.yonyou.iuap.baseservice.persistence.support;
-
-import com.yonyou.iuap.baseservice.entity.Model;
-
-public interface CustomDeletable<T extends Model> {
-    T getEntity();
-
-    int  doCustomDelete();
-}
