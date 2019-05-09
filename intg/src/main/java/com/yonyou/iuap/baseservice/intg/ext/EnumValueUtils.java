2150b3d7eaa00c3a9584dc5fa4d71c7dcd5be2fe
yangliangs <yangliangs@yonyou.com>
2019-05-09 19:58:45 +0800

    增加了支持国际化枚举反写的实现

---
+++ intg/src/main/java/com/yonyou/iuap/baseservice/intg/ext/EnumValueUtils.java
@@ -0,0 +1,44 @@
+package com.yonyou.iuap.baseservice.intg.ext;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 国际化枚举翻译工具
+ *
+ * @author leon
+ * @date 2019/3/14
+ * @since UCF1.0
+ */
+public class EnumValueUtils {
+    private static Logger logger = LoggerFactory.getLogger(EnumValueUtils.class);
+    /**
+     * 输出就靠这波了
+     * @param enumClass
+     * @return
+     * @throws NoSuchMethodException
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
+    public static Map loadEnumInfo(Class<? extends I18nEnum> enumClass)   {
+
+
+        Map map = new HashMap();
+        try {
+            Method method = enumClass.getMethod("values");
+            I18nEnum[] enumInst =  (I18nEnum[]) method.invoke(null, null);
+            map.putAll(enumInst[0].getMappings());
+        } catch (NoSuchMethodException|IllegalAccessException|InvocationTargetException e) {
+            logger.error("loading fail on enum :"+enumClass);
+        }
+        return map;
+    }
+
+
+
+}
---
+++ intg/src/main/java/com/yonyou/iuap/baseservice/intg/ext/I18nEnum.java
@@ -0,0 +1,15 @@
+package com.yonyou.iuap.baseservice.intg.ext;
+
+import java.util.Map;
+
+/**
+ * 枚举国际化,约定枚举国家化的反写值的范围
+ *
+ * @author leon
+ * @date 2019/3/14
+ * @since 3.5.6
+ */
+public interface I18nEnum {
+
+    Map getMappings();
+}
