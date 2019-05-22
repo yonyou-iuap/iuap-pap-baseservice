4f87242601fee676a6e11455fef1665e9e409152
yangliangs <yangliangs@yonyou.com>
2019-05-22 14:44:17 +0800

    clean 5.0 dependencies

--- core/src/main/java/com/yonyou/iuap/baseservice/controller/BaseController.java
+++ core/src/main/java/com/yonyou/iuap/baseservice/controller/BaseController.java
@@ -4,29 +4,29 @@ package com.yonyou.iuap.baseservice.controller;
 import com.yonyou.iuap.mvc.constants.RequestStatusEnum;
 import com.yonyou.iuap.mvc.type.JsonErrorResponse;
 import com.yonyou.iuap.mvc.type.JsonResponse;
-import com.yonyou.iuap.pap.base.i18n.MessageSourceUtil;
+import com.yonyou.iuap.ucf.common.i18n.MessageUtils;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang3.StringEscapeUtils;
+
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import net.sf.json.JSONObject;
-import org.apache.commons.lang.StringEscapeUtils;
-import org.apache.commons.lang.StringUtils;
-
 public class BaseController {
+    private final static String SUC = "success";
     public BaseController() {
     }
 
     public JsonResponse buildError(String field, String msg, RequestStatusEnum status) {
         JsonErrorResponse errorResponse = new JsonErrorResponse();
         if (RequestStatusEnum.SUCCESS.equals(status)) {
-            throw new IllegalArgumentException(MessageSourceUtil.getMessage("ja.bpm.bas.0001", "状态码设置错误!"));
+            throw new IllegalArgumentException("状态码设置错误!");
         } else {
             errorResponse.setSuccess(status.getStatus());
             if (RequestStatusEnum.FAIL_GLOBAL.equals(status)) {
-                errorResponse.setMessage(StringEscapeUtils.escapeHtml(msg));
+                errorResponse.setMessage(StringEscapeUtils.escapeHtml4 (msg));
             } else {
-                errorResponse.getDetailMsg().put(StringEscapeUtils.escapeHtml(field), StringEscapeUtils.escapeHtml(msg));
+                errorResponse.getDetailMsg().put(StringEscapeUtils.escapeHtml4(field), StringEscapeUtils.escapeHtml4(msg));
             }
 
             return errorResponse;
@@ -35,7 +35,7 @@ public class BaseController {
 
     public JsonResponse buildGlobalError(String msg) {
         JsonErrorResponse errorResponse = new JsonErrorResponse();
-        errorResponse.setMessage(StringEscapeUtils.escapeHtml(msg));
+        errorResponse.setMessage(StringEscapeUtils.escapeHtml4(msg));
         return errorResponse;
     }
 
@@ -49,14 +49,14 @@ public class BaseController {
     public JsonResponse buildError(Map<String, String> msgMap, RequestStatusEnum status) {
         JsonErrorResponse errorResponse = new JsonErrorResponse();
         if (RequestStatusEnum.SUCCESS.equals(status)) {
-            throw new IllegalArgumentException(MessageSourceUtil.getMessage("ja.bpm.bas.0001", "状态码设置错误!"));
+            throw new IllegalArgumentException(MessageUtils.getMessage("状态码设置错误!"));
         } else {
             errorResponse.setSuccess(status.getStatus());
             Iterator i$ = msgMap.entrySet().iterator();
 
             while(i$.hasNext()) {
                 Entry<String, String> entry = (Entry)i$.next();
-                errorResponse.getDetailMsg().put(StringEscapeUtils.escapeHtml((String)entry.getKey()), StringEscapeUtils.escapeHtml((String)entry.getValue()));
+                errorResponse.getDetailMsg().put(StringEscapeUtils.escapeHtml4(entry.getKey()), StringEscapeUtils.escapeHtml4(entry.getValue()));
             }
 
             return errorResponse;
@@ -71,14 +71,14 @@ public class BaseController {
 
     public <T> JsonResponse buildSuccess(Object value) {
         JsonResponse response = new JsonResponse();
-        response.setMessage(MessageSourceUtil.getMessage("ja.bpm.bas.0002", "操作成功"));
+        response.setMessage(SUC);
         response.getDetailMsg().put("data", value);
         return response;
     }
 
     public <T> JsonResponse buildSuccess() {
         JsonResponse response = new JsonResponse();
-        response.setMessage(MessageSourceUtil.getMessage("ja.bpm.bas.0002", "操作成功"));
+        response.setMessage(SUC);
         return response;
     }
 
@@ -90,15 +90,15 @@ public class BaseController {
 
     public JSONObject buildJsonSuccess(String msg) {
         JSONObject json = new JSONObject();
-        json.put("flag", "success");
-        json.put("msg", StringUtils.isEmpty(msg) ? MessageSourceUtil.getMessage("ja.bpm.bas.0003", "操作成功!") : msg);
+        json.put("flag", SUC);
+        json.put("msg",  msg);
         return json;
     }
 
     public JSONObject buildJsonFail(String msg) {
         JSONObject json = new JSONObject();
         json.put("flag", "fail");
-        json.put("msg", StringUtils.isEmpty(msg) ? MessageSourceUtil.getMessage("ja.bpm.bas.0004", "操作失败!") : msg);
+        json.put("msg",  msg);
         return json;
     }
 
