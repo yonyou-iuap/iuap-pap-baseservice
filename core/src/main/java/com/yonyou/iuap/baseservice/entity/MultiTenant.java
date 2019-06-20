2283ae3196769df1ebf9cb30f4f236a4e06755f9
yangliangs <yangliangs@yonyou.com>
2019-06-20 14:02:24 +0800

    还原了tenantid的大小写声明,并新建了一个接口用于标准化tennatId的名称

--- core/src/main/java/com/yonyou/iuap/baseservice/entity/MultiTenant.java
+++ core/src/main/java/com/yonyou/iuap/baseservice/entity/MultiTenant.java
@@ -2,10 +2,12 @@ package com.yonyou.iuap.baseservice.entity;
 
 
 /**
- * 说明：多租户基础Model
+ * 说明：多租户基础Model,已废弃
+ * 推荐使用新版的TenantId
  * @author jhb
  * 2018年8月8日
  */
+@Deprecated
 public interface MultiTenant extends Model {
     public String getTenantid();
     public void setTenantid(String tenantid);
